463a2c21e0c81f6ab07ef1c498b40d08153ed050
Kashfia Meherin <kashfiameherin2k23@gmail.com>
2026-05-09 23:17:55 +0600

    Creating avatar and sign-Out button in navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,19 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import logo from "../../../public/assets/logo.png"
+import logo from "../../../public/assets/logo.png";
+import { authClient } from "@/lib/auth-client";
+import { Avatar } from "@heroui/react";
+import { Button } from "@heroui/react";
 
 const Navbar = () => {
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
+
+  const handleSignOut = async() =>{
+    await authClient.signOut()
+  }
+
   return (
     <div className="border-b px-2">
       <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
@@ -35,14 +45,31 @@ const Navbar = () => {
         </ul>
 
         <div className="flex gap-4">
-          <ul className="flex items-center gap-5  text-sm">
-            <li>
-              <Link href={"/signup"}>SignUp</Link>
-            </li>
-            <li>
-              <Link href={"/signin"}>SignIn</Link>
-            </li>
-          </ul>
+          {!user && (
+            <ul className="flex items-center gap-5  text-sm">
+              <li>
+                <Link href={"/signup"}>SignUp</Link>
+              </li>
+              <li>
+                <Link href={"/signin"}>SignIn</Link>
+              </li>
+            </ul>
+          )}
+
+          {user && (
+            <div className="flex gap-4">
+              <Avatar size="sm">
+                <Avatar.Image
+                  alt="John Doe"
+                  src={user?.image}
+                  referrerPolicy="no-referrer"
+                />
+                <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
+              </Avatar>
+
+              <Button onClick={handleSignOut} variant="danger" size="sm">Sign Out</Button>
+            </div>
+          )}
         </div>
       </nav>
     </div>
